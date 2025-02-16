470681fd5c1574b663048573e9c2fc34f91a105a
Mahak Gupta <mahakgupta10a24@gmail.com>
2025-02-16 17:17:39 +0530

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const App=()=>{
   return(
-  <div class="text-3xl font-bold underline">hello</div>)
+    <div class="wrapper">
+        <div class="logo">
+          <h1>logo</h1>
+        </div>
+        <div class="buttons">
+          <button class="same">github</button>
+          <button class="same1">sign in</button>
+          <button class="same">sign up</button>
+        </div>
+
+    </div>
+)
 }
 
 export default App
