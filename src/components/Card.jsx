e1fdd2a1736d6f8213680585d5f426698a66fb84
Mahak Gupta <mahakgupta10a24@gmail.com>
2025-02-19 21:54:02 +0530

    change

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,8 +4,7 @@ const Card = () => {
   return (
     <div class="card">
         <h2>Personalised study platform</h2>
-        <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et inventore eligendi voluptate sit eos odio doloremque debitis 
-          </h5>
+        <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et inventore eligendi voluptate sit eos odio doloremque debitis</h5>
     </div>
   )
 }
