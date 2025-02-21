ad54c9446fb8b0951f3eb58f38791159b1cf956f
Mahak Gupta <mahakgupta10a24@gmail.com>
2025-02-21 20:36:00 +0530

    button added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,6 +5,7 @@ const Card = () => {
     <div class="card">
         <h2>Personalised study platform</h2>
         <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et inventore eligendi voluptate sit eos odio doloremque debitis</h5>
+        <button class="hi">Hi there!</button>
     </div>
   )
 }
