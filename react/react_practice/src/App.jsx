95bb57388aa7525ed7e818237f48df66df15766f
Mahak Gupta <mahakgupta10a24@gmail.com>
2025-07-29 18:32:32 +0530

    added counter constraints

--- react/react_practice/src/App.jsx
+++ react/react_practice/src/App.jsx
@@ -2,18 +2,40 @@ import { useState } from "react";
 
 function App() {
   // let counter=0;
-  let [counter,setCounter]=useState(0)
+  let [counter, setCounter] = useState(0);
 
   return (
     <>
-     <h1>Counter value: {counter}</h1>
-     <button onClick={()=>{counter=counter + 1;
-      setCounter(counter); console.log(counter)}}>Add value</button>
-     <button onClick={()=>{counter=counter - 1;
-      setCounter(counter)
-     }}>decrease value</button>
+      <h1>Counter value: {counter}</h1>
+      <button
+        onClick={() => {
+          counter = counter + 1;
+          if (counter == 20) {
+            alert("you're exceeding value");
+            setCounter(20);
+          } else {
+            setCounter(counter);
+          }
+        }}
+      >
+        Add value
+      </button>
+
+      <button
+        onClick={() => {
+          counter = counter - 1;
+          if (counter ==0){
+            alert("you're going in negative counter");
+            setCounter(0);
+          } else {
+            setCounter(counter);
+          }
+        }}
+      >
+        decrease value
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
