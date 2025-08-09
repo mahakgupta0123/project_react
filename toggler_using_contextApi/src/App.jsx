eebd945ea1da1c50f26dcde539c0c81aee14bed7
Mahak Gupta <mahakgupta10a24@gmail.com>
2025-08-09 16:54:36 +0530

    Added new context api project

---
+++ toggler_using_contextApi/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='p-4 bg-red-400 rounded border'>hello world!</div>
+    </>
+  )
+}
+
+export default App
